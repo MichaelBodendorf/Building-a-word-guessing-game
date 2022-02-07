e1ec1ec9bd864dd32b41fb64bb29e3ca915eeecb
MichaelBodendorf <bodi@gmx.at>
2022-02-07 10:48:36 +0100

    Part II

--- app/src/main/java/game/WordChooser.java
+++ app/src/main/java/game/WordChooser.java
@@ -2,7 +2,9 @@ package game;
 import java.util.Random;
 
 public class WordChooser {
+  
   private final String[] DICTIONARY = {"MAKERS", "CANDIES", "DEVELOPER", "LONDON"};
+  
   public String getRandomWordFromDictionary() {
     Random rand = new Random();
     return DICTIONARY[rand.nextInt(DICTIONARY.length)];
